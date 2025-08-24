d892bcdfd20488d292dfa130ee8dcac022462f70
Ben Taylor <ben.mark.taylor@gmail.com>
2025-08-24 17:48:51 +0300

    trying to backup to earlier android version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover, interactive-widget=resizes-content" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
           <meta name="theme-color" content="#ffffff" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-tap-highlight" content="no" />
